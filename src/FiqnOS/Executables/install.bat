7f9365deb9e79c4aac8d178a8e236b6671716523
e1uen <110753825+zipmishahl2@users.noreply.github.com>
2024-09-25 16:17:09 +0700

    Update install.bat

--- src/FiqnOS/Executables/install.bat
+++ src/FiqnOS/Executables/install.bat
@@ -1,6 +1,12 @@
 @echo off
 CD /d %~dp0
 
-mkdir %WINDIR%\Temp\FiqnOS
-curl -L -o %WINDIR%\Temp\FiqnOS\VisualCppRedist_AIO_x86_x64.exe https://github.com/abbodi1406/vcredist/releases/download/v0.81.0/VisualCppRedist_AIO_x86_x64.exe
-start %WINDIR%\Temp\FiqnOS\VisualCppRedist_AIO_x86_x64.exe /ai
+timeout /t 2 >nul
+curl -g -k -L -# -o "C:\VisualCppRedist_AIO_x86_x64.exe" "https://github.com/abbodi1406/vcredist/releases/download/v0.83.0/VisualCppRedist_AIO_x86_x64.exe"
+start /wait C:\VisualCppRedist_AIO_x86_x64.exe /ai
+del /F /Q C:\VisualCppRedist_AIO_x86_x64.exe
+
+timeout /t 2 >nul
+curl -g -k -L -# -o "C:\dxwebsetup.exe" "https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe"
+start /wait C:\dxwebsetup.exe /Q
+del /F /Q C:\dxwebsetup.exe
