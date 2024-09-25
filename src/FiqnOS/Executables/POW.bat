83cf2320731485daafd70a5f865fb27fa0b47852
e1uen <110753825+zipmishahl2@users.noreply.github.com>
2024-09-25 16:47:58 +0700

    Update POW.bat

--- src/FiqnOS/Executables/POW.bat
+++ src/FiqnOS/Executables/POW.bat
@@ -1,5 +1,5 @@
 @echo off
-powercfg -import "C:\Windows\APIs\POW\fiqn.pow"
+powercfg -import "C:\Windows\APIs\POW\fiqn.pow" 115e7fd6-e9b1-4a76-9285-a642348a675c
 powercfg /setactive 115e7fd6-e9b1-4a76-9285-a642348a675c
 powercfg /setactive scheme_current
 powercfg -h off
