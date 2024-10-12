4be8e9068c85db8e73a9ed546055b6b1f70496db
e1uen <110753825+e1uen@users.noreply.github.com>
2024-10-12 18:29:46 +0700

    Update FINALIZE.bat

--- src/FiqnOS/Executables/FINALIZE.bat
+++ src/FiqnOS/Executables/FINALIZE.bat
@@ -17,36 +17,20 @@ for %%a in (
 	WdfDirectedPowerTransitionEnable
 	EnableIdlePowerManagement
 	IdleInWorkingState
-	fid_D1Latency
-	fid_D2Latency
-	fid_D3Latency
 	IoLatencyCap
 	DmaRemappingCompatible
 	DmaRemappingCompatibleSelfhost
 ) do for /f "delims=" %%b in ('reg query "HKLM\SYSTEM\CurrentControlSet\Enum" /s /f "%%b" ^| findstr "HKEY"') do reg add "%%b" /v "%%b" /t REG_DWORD /d "0" /f >nul
 
-bcdedit /set disabledynamictick yes
-bcdedit /deletevalue useplatformclock
-bcdedit /deletevalue useplatformtick
-bcdedit /set noumex Yes
-bcdedit /set bootems No
-bcdedit /set ems No
-bcdedit /set bootlog No
-bcdedit /set hypervisorlaunchtype No
-bcdedit /set isolatedcontext No
-bcdedit /set vsmlaunchtype Off
-bcdedit /set vm No
-bcdedit /set quietboot Yes
-bcdedit /set integrityservices disable
-bcdedit /set nx optin
-bcdedit /set pae ForceDisable
-bcdedit /set x2apicpolicy Enable
-bcdedit /set bootux Disabled
-bcdedit /set halbreakpoint No
-bcdedit /set bootmenupolicy legacy
-bcdedit /set tscsyncpolicy Enhanced
-bcdedit /set uselegacyapicmode No
-bcdedit /set loadoptions DISABLE-LSA-ISO,DISABLE-VBS
+bcdedit /set hypervisorlaunchtype No >nul
+bcdedit /set isolatedcontext No >nul
+bcdedit /set vsmlaunchtype Off >nul
+bcdedit /set vm No >nul
+bcdedit /set quietboot Yes >nul
+bcdedit /set integrityservices disable >nul
+bcdedit /set nx AlwaysOff >nul
+bcdedit /set bootux Disabled >nul
+bcdedit /set bootmenupolicy legacy >nul
 bcdedit /set {current} description "FiqnOS"
 label C: FiqnOS
 cls
@@ -60,36 +44,19 @@ reg add "HKEY_CURRENT_USER\Control Panel\Mouse" /v "MouseSensitivity" /t REG_SZ 
 reg add "HKEY_USERS\.DEFAULT\Control Panel\Mouse" /v "MouseSpeed" /t REG_SZ /d "0" /f >nul
 reg add "HKEY_USERS\.DEFAULT\Control Panel\Mouse" /v "MouseThreshold1" /t REG_SZ /d "0" /f >nul
 reg add "HKEY_USERS\.DEFAULT\Control Panel\Mouse" /v "MouseThreshold2" /t REG_SZ /d "0" /f >nul
-powershell disable-netadapterbinding -Name "*" -componentid "vmware_brige, ms_lldp, ms_lltdio, ms_implat, ms_tcpip6, ms_rspndr, ms_server, ms_msclient"
 reg delete "HKLM\System\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f && reg add "HKLM\System\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f >nul 2>&1
-Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\Tpm" /v "Start" /t REG_DWORD /d "0" /f >nul
-Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\WinPhoneCritical" /v "Start" /t REG_DWORD /d "0" /f >nul
 Reg.exe add "HKCU\Software\Microsoft\Office\Common\ClientTelemetry" /v "DisableTelemetry" /t REG_DWORD /d "1" /f
-
-setlocal
-
-set autologgers=autologger-diagtrack-listener,cellcore,cloudexperiencehostoobe,lwtnetlog,melanos-Kernel,microsoft-windows-assignedacces-trace,microsoft-windows-rdp-graphics-rdpidd-trace,microsoft-windows-setup,netcore,ntfslog,peauthlog,radiomgr,readyboot,refslog,setupplatform,setupplatformtel,spoolerlogger,tcpiplogger,wifisession,wifidriverhvsessionrepro,wifidriverihvsession,wfp-ipsec-trace,ubpm,tilestore
-
-for %%Q in (%autologgers%) do (
-    powershell -Command "Remove-AutologgerConfig -Name %%~Q"
-)
-
-endlocal
 
 PowerShell -NonInteractive -NoLogo -NoProfile -Command "Disable-MMAgent -mc | Disable-WindowsErrorReporting | Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi | ForEach-Object { $_.enable = $false; $_.psbase.put(); }"
 
 powershell -NoProfile -Command "Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6, ms_msclient, ms_server, ms_rspndr, ms_lltdio, ms_implat, ms_lldp"
 PowerShell -command "Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi | ForEach-Object { $_.enable = $false; $_.psbase.put(); }"
 
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SysMain" /v "start" /t REG_DWORD /d "4" /f
-
 for /f "delims=" %%u in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces" /s /f "NetbiosOptions" ^| findstr "HKEY"') do (
     reg add "%%u" /v "NetbiosOptions" /t REG_DWORD /d "2" /f
 )
 
-fsutil behavior set disable8dot3 1
-fsutil behavior set disablelastaccess 1
-fsutil behavior set disabledeletenotify 0
-fsutil behavior set encryptpagingfile 0
-fsutil behavior set disablecompression 1
-fsutil behavior set memoryusage 2
+fsutil behavior set disable8dot3 1 >nul
+fsutil behavior set disablelastaccess 1 >nul
+fsutil behavior set disabledeletenotify 0 >nul
+fsutil behavior set memoryusage 2 >nul
